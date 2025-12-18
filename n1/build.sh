2f538bc0bef2c4ba1106a80b7c99669ab26bb238
tuhaiyang <39900390+tuhaiyang@users.noreply.github.com>
2025-12-18 20:24:49 +0800

    Update build.sh

--- n1/build.sh
+++ n1/build.sh
@@ -31,6 +31,7 @@ PACKAGES="$PACKAGES luci-i18n-vlmcsd-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-upnp-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-openvpn-zh-cn"
 PACKAGES="$PACKAGES luci-app-adguardhome"
+PACKAGES="$PACKAGES luci-i18n-qbittorrent-zh-cn"
 # 文件管理器
 PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
 # 静态文件服务器dufs(推荐)
