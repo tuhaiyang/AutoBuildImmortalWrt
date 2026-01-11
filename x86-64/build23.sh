e35f24edce35db4cd433c8a74e0a1ac5e5d1ece4
tuhaiyang <39900390+tuhaiyang@users.noreply.github.com>
2026-01-11 13:39:14 +0800

    Update build23.sh

--- x86-64/build23.sh
+++ x86-64/build23.sh
@@ -57,6 +57,9 @@ PACKAGES="$PACKAGES luci-app-openclash"
 PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
 PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-vlmcsd-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-upnp-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-passwall2-zh-cn"
 # 静态文件服务器dufs(推荐)
 PACKAGES="$PACKAGES luci-i18n-dufs-zh-cn"
 # ======== shell/custom-packages.sh =======
